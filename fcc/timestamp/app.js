24bb24eca329097cc5e1e9d574af76ad26eb15b6
Seyy <aitbihiyanis@gmail.com>
2024-10-18 17:33:57 +0200

    2nd add fcc

---
+++ fcc/timestamp/app.js
@@ -0,0 +1,44 @@
+import dotenv from 'dotenv'
+import express from 'express';
+import path from 'path'
+import { fileURLToPath } from 'url';
+
+dotenv.config();
+const app = express();
+app.set('trust proxy', true);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/api/:param', (req, res) => {
+    const userParam = req.params.param;
+    if (userParam.includes('-')) {
+
+        const date = new Date(userParam);
+        const correctDate = date.toUTCString();
+        const unixDate = Date.parse(correctDate);
+        res.json({
+            unix: unixDate,
+            utc: correctDate 
+        });
+
+    } else {
+        const inted = parseInt(userParam);
+        const date = new Date(inted);
+        const correctDate = date.toUTCString();
+        res.json({
+            unix: inted,
+            utc: correctDate
+        });
+
+    }
+});
+
+app.listen(port, () => {
+    console.log(`connecté sur le port : ${port}`);
+});
